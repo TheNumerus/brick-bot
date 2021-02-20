807f3023afb7b853afc55d1e395adf39fcbae769
Petr Volf <Petr.volf216@gmail.com>
2021-02-20 15:01:04 +0100

    reconnect should NOW be working, small refactor

--- src/bin/ws_api.rs
+++ src/bin/ws_api.rs
@@ -16,14 +16,20 @@ use reqwest::{
 };
 use serde_repr::*;
 
-use futures_channel::mpsc::TrySendError;
 use serde::{de::DeserializeOwned, Deserialize};
 
 use anyhow::{Context, Result};
 
-use futures_util::StreamExt;
+use futures_util::{SinkExt, StreamExt};
 use serde_json::json;
-use tokio::{task::JoinHandle, time::sleep};
+use tokio::{
+    sync::{
+        mpsc::{error::SendError, unbounded_channel},
+        Mutex,
+    },
+    task::JoinHandle,
+    time::sleep,
+};
 use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
 
 use brick_bot::{
@@ -41,17 +47,12 @@ pub static BRICK_GIF: OnceCell<Vec<u8>> = OnceCell::new();
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let config = tokio::fs::read_to_string("bot.toml").await.context("Error loading bot configuration.")?;
-    let config: Config = toml::from_str(&config).context("Could not parse settings")?;
-    // It's safe to unwrap string options now
-    let config = config.set_missing();
+    let config = prepare_config().await?;
 
     let client = reqwest::Client::new();
 
     set_token(&config.token)?;
 
-    let identified = Arc::new(AtomicBool::from(false));
-    let sequence_number = Arc::new(AtomicUsize::from(0));
     BRICK_GIF
         .set(tokio::fs::read(&config.image_path).await.context("cannot find image on given path")?)
         .unwrap();
@@ -59,54 +60,122 @@ async fn main() -> Result<()> {
     let me: DiscordResult<User> = get_json(&client, "https://discord.com/api/users/@me").await?;
     let my_id = Result::from(me)?.id;
 
-    let mut cache = AvatarCache::new();
+    let cache = Arc::new(Mutex::new(AvatarCache::new()));
 
-    let (tx, rx) = futures_channel::mpsc::unbounded();
+    // will enter enother iteration only when discord needs another connection
+    loop {
+        let (tx, mut rx) = unbounded_channel();
 
-    let (ws_stream, _ws_res) = connect_async("wss://gateway.discord.gg/?v=8&encoding=json").await.expect("Failed to connect");
-    println!("WebSocket handshake has been successfully completed");
+        let (ws_stream, _ws_res) = connect_async("wss://gateway.discord.gg/?v=8&encoding=json").await.expect("Failed to connect");
+        log_message("WebSocket connected");
 
-    let (write, mut read) = ws_stream.split();
+        let (mut write, mut read) = ws_stream.split();
 
-    let sender = rx.map(Ok).forward(write);
-    tokio::pin!(sender);
+        let sender = tokio::spawn(async move {
+            while let Some(m) = rx.recv().await {
+                write.send(m).await;
+            }
+        });
 
-    let config_clone = config.clone();
-    let ws_to_stdout = tokio::spawn(async move {
-        // TODO use option
-        let mut session_id = String::from("");
-        loop {
-            let tx = tx.clone();
-            match read.next().await {
-                Some(message) => {
-                    println!("{:#?}", message);
-                    let msg = message.unwrap().to_string();
-                    let payload: Payload = serde_json::from_str(&msg).unwrap();
-                    println!("{:#?}", payload);
-                    if payload.op == Opcode::Hello {
-                        let heartbeat = payload.d.unwrap()["heartbeat_interval"].as_u64().unwrap();
-                        // TODO not handling reconnections for now
-                        let seq_num = Arc::clone(&sequence_number);
-                        let heartbeater = tokio::spawn(async move {
-                            loop {
-                                let hb_message = json! ({
-                                    "op": Opcode::Heartbeat,
-                                    "d": seq_num.load(Ordering::SeqCst)
-                                });
-                                tx.unbounded_send(Message::text(hb_message.to_string()))?;
-                                println!("sent heartbeat");
-                                sleep(Duration::from_millis(heartbeat)).await;
+        let identified = Arc::new(AtomicBool::from(false));
+        let sequence_number = Arc::new(AtomicUsize::from(0));
+
+        let config = Arc::clone(&config);
+        let cache = Arc::clone(&cache);
+        let my_id = my_id.clone();
+        let client = client.clone();
+
+        let ws_to_stdout = tokio::spawn(async move {
+            // TODO use option
+            let mut heartbeater: Option<JoinHandle<Result<(), SendError<Message>>>> = None;
+            let mut session_id = String::from("");
+            let config = Arc::clone(&config);
+            loop {
+                let tx = tx.clone();
+                match read.next().await {
+                    Some(message) => {
+                        println!("{:#?}", message);
+                        let msg = message.unwrap().to_string();
+                        let payload: Payload = serde_json::from_str(&msg).unwrap();
+                        println!("{:#?}", payload);
+                        if payload.op == Opcode::Hello {
+                            let heartbeat = payload.d.unwrap()["heartbeat_interval"].as_u64().unwrap();
+                            // TODO not handling reconnections for now
+                            let seq_num = Arc::clone(&sequence_number);
+                            heartbeater = Some(tokio::spawn(async move {
+                                loop {
+                                    let hb_message = json! ({
+                                        "op": Opcode::Heartbeat,
+                                        "d": seq_num.load(Ordering::SeqCst)
+                                    });
+                                    tx.send(Message::text(hb_message.to_string()))?;
+                                    println!("sent heartbeat");
+                                    sleep(Duration::from_millis(heartbeat)).await;
+                                }
+                            }));
+                        } else if payload.op == Opcode::HeartbeatAck {
+                            if !identified.load(Ordering::SeqCst) {
+                                // TODO add activity to config
+                                let identify = json!({
+                                    "op": Opcode::Identify,
+                                    "d": {
+                                        "token": &config.token,
+                                        "intents": 512,
+                                        "properties": {
+                                            "$os": "windows",
+                                            "$browser": "brick-bot",
+                                            "$device": "brick-bot"
+                                        },
+                                        "presence": {
+                                            "activities": [{
+                                                "name": "Brickity brick 🧱",
+                                                "type": 0
+                                            }],
+                                            "status": "online",
+                                            "since": 0,
+                                            "afk": false
+                                        },
+                                    }
+                                })
+                                .to_string();
+                                tx.send(Message::Text(identify.to_string())).unwrap();
+                                identified.store(true, Ordering::SeqCst);
                             }
-                            // TODO warning, but error when line is deleted
-                            Ok::<(), TrySendError<Message>>(())
-                        });
-                    } else if payload.op == Opcode::HeartbeatAck {
-                        if !identified.load(Ordering::SeqCst) {
+                        } else if payload.op == Opcode::Dispatch {
+                            let s = payload.s.to_owned().unwrap();
+                            sequence_number.store(s, Ordering::SeqCst);
+                            if let Some(event) = payload.t {
+                                if event == "MESSAGE_CREATE" {
+                                    let message: DiscordMessage = serde_json::from_value(payload.d.unwrap()).unwrap();
+
+                                    // clone everything needed
+                                    let config = Arc::clone(&config);
+                                    let client = client.clone();
+                                    let cache = Arc::clone(&cache);
+                                    let my_id = my_id.clone();
+
+                                    tokio::spawn(async move {
+                                        on_message_create(message, config, client, cache, &my_id).await?;
+                                        Ok::<(), BotError>(())
+                                    });
+                                } else if event == "READY" {
+                                    // TODO cleanup
+                                    session_id = payload.d.clone().unwrap()["session_id"].as_str().unwrap().to_owned();
+                                }
+                            }
+                        } else if payload.op == Opcode::InvalidSession {
+                            let resumable = payload.d.unwrap().as_bool().unwrap();
+                            if !resumable {
+                                // Reset connection
+                                heartbeater.take().unwrap().abort();
+                                return Ok(());
+                            }
+                            // TODO dedup code
                             // TODO add activity to config
                             let identify = json!({
                                 "op": Opcode::Identify,
                                 "d": {
-                                    "token": &config_clone.token,
+                                    "token": &config.token,
                                     "intents": 512,
                                     "properties": {
                                         "$os": "windows",
@@ -125,114 +194,96 @@ async fn main() -> Result<()> {
                                 }
                             })
                             .to_string();
-                            tx.unbounded_send(Message::Text(identify.to_string())).unwrap();
+                            tx.send(Message::Text(identify.to_string())).unwrap();
+                            identified.store(true, Ordering::SeqCst);
+                        } else if payload.op == Opcode::Reconnect {
+                            // TODO dedup code
+                            let identify = json!({
+                                "op": Opcode::Resume,
+                                "d": {
+                                    "token": &config.token,
+                                    "session_id": &session_id,
+                                    "seq": sequence_number.load(Ordering::SeqCst),
+                                }
+                            })
+                            .to_string();
+                            tx.send(Message::Text(identify.to_string())).unwrap();
                             identified.store(true, Ordering::SeqCst);
                         }
-                    } else if payload.op == Opcode::Dispatch {
-                        let s = payload.s.to_owned().unwrap();
-                        sequence_number.store(s, Ordering::SeqCst);
-                        if let Some(event) = payload.t {
-                            if event == "MESSAGE_CREATE" {
-                                let message: DiscordMessage = serde_json::from_value(payload.d.unwrap()).unwrap();
-
-                                if !message.content.starts_with(&config.command) {
-                                    continue;
-                                }
-
-                                // send error messeage
-                                if message.mentions.is_empty() {
-                                    match message.mention_roles {
-                                        Some(roles) if !roles.is_empty() => {
-                                            let res = send_reply(&client, &message.channel_id, &message.id, &config.err_msg_tag_role.clone().unwrap()).await?;
-                                            Result::from(res)?;
-                                            log_message("Error - tagged role");
-                                        }
-                                        _ => {
-                                            let res =
-                                                send_reply(&client, &message.channel_id, &message.id, &config.err_msg_tag_nobody.clone().unwrap()).await?;
-                                            Result::from(res)?;
-                                            log_message("Error - tagged nobody");
-                                        }
-                                    }
-                                }
-
-                                for user in message.mentions {
-                                    if user.id == my_id {
-                                        if let Some(ref self_message) = config.self_brick_message {
-                                            send_reply(&client, &message.channel_id, &message.id, self_message).await?;
-                                            log_message(format!("Bricked self"));
-                                            continue;
-                                        }
-                                    }
-
-                                    let avatar = cache.get(&client, &user).await?;
-
-                                    let image = brickify_gif(BRICK_GIF.get().unwrap(), avatar, &config).await?;
-
-                                    let image_res: DiscordResult<DiscordMessage> = send_image(&client, &message.channel_id, &image, &config).await?;
-                                    Result::from(image_res)?;
-
-                                    log_message(format!("Bricked user \"{}\"", user.username));
-                                }
-                            } else if event == "READY" {
-                                // TODO cleanup
-                                session_id = payload.d.clone().unwrap()["session_id"].as_str().unwrap().to_owned();
-                            }
-                        }
-                    } else if payload.op == Opcode::InvalidSession {
-                        // TODO dedup code
-                        // TODO add activity to config
-                        let identify = json!({
-                            "op": Opcode::Identify,
-                            "d": {
-                                "token": &config_clone.token,
-                                "intents": 512,
-                                "properties": {
-                                    "$os": "windows",
-                                    "$browser": "brick-bot",
-                                    "$device": "brick-bot"
-                                },
-                                "presence": {
-                                    "activities": [{
-                                        "name": "Brickity brick 🧱",
-                                        "type": 0
-                                    }],
-                                    "status": "online",
-                                    "since": 0,
-                                    "afk": false
-                                },
-                            }
-                        })
-                        .to_string();
-                        tx.unbounded_send(Message::Text(identify.to_string())).unwrap();
-                        identified.store(true, Ordering::SeqCst);
-                    } else if payload.op == Opcode::Reconnect {
-                        // TODO dedup code
-                        // TODO add activity to config
-                        let identify = json!({
-                            "op": Opcode::Resume,
-                            "d": {
-                                "token": &config_clone.token,
-                                "session_id": &session_id,
-                                "seq": sequence_number.load(Ordering::SeqCst),
-                            }
-                        })
-                        .to_string();
-                        tx.unbounded_send(Message::Text(identify.to_string())).unwrap();
-                        identified.store(true, Ordering::SeqCst);
+                    }
+                    None => {
+                        break;
                     }
                 }
-                None => {
-                    break;
-                }
+            }
+            Ok::<(), BotError>(())
+        });
+
+        // TODO this feels wrong
+        tokio::try_join! {sender, ws_to_stdout};
+    }
+}
+
+/// Called when bot recieves message
+async fn on_message_create(
+    message: DiscordMessage,
+    config: Arc<Config>,
+    client: Client,
+    avatar_cache: Arc<Mutex<AvatarCache>>,
+    my_id: &str,
+) -> Result<(), BotError> {
+    if !message.content.starts_with(&config.command) {
+        return Ok(());
+    }
+
+    // send error messeage
+    if message.mentions.is_empty() {
+        match message.mention_roles {
+            Some(roles) if !roles.is_empty() => {
+                let res = send_reply(&client, &message.channel_id, &message.id, &config.err_msg_tag_role.clone().unwrap()).await?;
+                Result::from(res)?;
+                log_message("Error - tagged role");
+            }
+            _ => {
+                let res = send_reply(&client, &message.channel_id, &message.id, &config.err_msg_tag_nobody.clone().unwrap()).await?;
+                Result::from(res)?;
+                log_message("Error - tagged nobody");
             }
         }
-        Ok::<(), BotError>(())
-    });
+    }
 
-    // TODO this feels wrong
-    tokio::join! {sender, ws_to_stdout};
+    // brick everyone mentioned
+    for user in message.mentions {
+        if user.id == my_id {
+            if let Some(ref self_message) = config.self_brick_message {
+                send_reply(&client, &message.channel_id, &message.id, self_message).await?;
+                log_message(format!("Bricked self"));
+                continue;
+            }
+        }
+
+        let avatar = {
+            let mut lock = avatar_cache.lock().await;
+            let avatar = lock.get(&client, &user).await?.clone();
+            avatar
+        };
+
+        let image = brickify_gif(BRICK_GIF.get().unwrap(), &avatar, &config).await?;
+
+        let image_res: DiscordResult<DiscordMessage> = send_image(&client, &message.channel_id, &image, &config).await?;
+        Result::from(image_res)?;
+
+        log_message(format!("Bricked user \"{}\"", user.username));
+    }
+
     Ok(())
+}
+
+async fn prepare_config() -> Result<Arc<Config>> {
+    let config = tokio::fs::read_to_string("bot.toml").await.context("Error loading bot configuration.")?;
+    let config: Config = toml::from_str(&config).context("Could not parse settings")?;
+    // It's safe to unwrap string options now
+    Ok(Arc::from(config.set_missing()))
 }
 
 // TODO move
